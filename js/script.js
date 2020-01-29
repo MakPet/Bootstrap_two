5af49fa2ed5650ea7a4c9a6b0e68944ec80c9e10
MakPet <valadok123@gmail.com>
2020-01-29 22:47:05 +0200

    Настройки слайдера

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
-$('.about-us__slider').slick({
-  	infinite: false,
-  	slidesToShow: 3,
-  	slidesToScroll: 1
+$(document).ready(function(){
+  	$('.about-us__slider').slick({
+  		infinite: false,
+  		slidesToShow: 4,
+  		slidesToScroll: 1,
+  		prevArrow:"<div class='about-us__slider__arrow about-us__slider__arrow_prev text-center icon-angle-left'></div>",
+		nextArrow:"<div class='about-us__slider__arrow about-us__slider__arrow_next text-center icon-angle-right'></div>",
+		autoplay: false,
+		speed: 1500,
+
+		responsive: [
+	    	{
+	    		breakpoint: 1200,
+	    		settings: {
+	    			slidesToShow: 3
+	    		}
+	    	},
+	    	{
+	    		breakpoint: 992,
+	    		settings: {
+	    			slidesToShow: 2
+	    		}
+	    	},
+	    	{
+	    		breakpoint: 768,
+	    		settings: {
+	    			slidesToShow: 1,
+	    			arrows: false,
+	    			infinite: true,
+	    			autoplay: true,
+	    			autoplaySpeed: 2000
+	    		}
+	    	}
+	    ]
+	});
 });
