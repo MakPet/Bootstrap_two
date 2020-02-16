4c32be7d33ac7f818d51c0f85ed3c8f61b65dcdd
MakPet <valadok123@gmail.com>
2020-02-16 12:28:58 +0200

    Рабочий фильтр в блоке lates work

--- js/script.js
+++ js/script.js
@@ -10,18 +10,22 @@ $(document).ready(function(){
 	
 	
 	$(function() {
-		// create an empty variable
-		var selectedClass = "";
-		// call function when item is clicked
-		$(".lastest-work__nav-link").click(function(){
-			// assigns class to selected item
-			selectedClass = $(this).attr("data-rel");
-			// fades out all portfolio items
-			$(".demonstration-lastest-work .demonstration-lastest-work__inner").fadeOut(300);
-			// fades in selected category
-			$(".demonstration-lastest-work .demonstration-lastest-work__inner strategy" + selectedClass).delay(300).fadeIn(300);
-		});
-	});
+    // create an empty variable
+    var selectedClass = "";
+    // call function when item is clicked
+    $(".lastest-work__nav-link").click(function(){
+      // assigns class to selected item
+      selectedClass = $(this).attr("data-rel");
+      // hide all portfolio items
+      $(".demonstration-lastest-work .demonstration-lastest-work__inner").fadeOut(300);
+      // show selected category
+      if(selectedClass==='all'){
+        $(".demonstration-lastest-work__inner").delay(300).fadeIn(300);
+      } else {
+        $("." + selectedClass).delay(300).fadeIn(300);
+      }
+    });
+  });
   
 }); // document ready
 
